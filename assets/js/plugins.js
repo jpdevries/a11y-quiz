45d1c29342622643b4ef8e2843e202a2382f4cf3
JP DeVries <mail@devries.jp>
2016-07-05 00:58:24 +0200

    Node server, sync form submissions
    woohoo!

---
+++ assets/js/plugins.js
@@ -0,0 +1,40 @@
+var bank = {
+  "does-a11y-rock":"yes",
+  "a11y-stand-for":"remind",
+  "should-all-access":"yes",
+  "least-semantic":"div",
+  "which-country":"india",
+  "is-enhanced":"yes",
+  "sync-endpoints":"yes",
+  "percent-nojs":"18",
+  "a11y-minded-aim-to":"justice",
+  //"seo":"1",
+  //"performance":"1",
+  //"optimization":"1",
+  //"usability":"1",
+  //"ux":"1"
+};
+
+function calculateScore(userInput,bank) {
+  var score = 0;
+  var checkAll = 0;
+  for(var keyvalue of userInput) {
+    if((keyvalue[0] == "seo" || keyvalue[0] == "performance" || keyvalue[0] == "optimization" || keyvalue[0] == "usability" || keyvalue[0] == "ux") && keyvalue[1] == "1") {
+        checkAll++;
+    }
+    else if(bank[keyvalue[0]] == keyvalue[1]) score++;
+  }
+  if(checkAll >= 5) score++;
+  
+  return score;
+}
+
+function scoreMessage(score,numQuestions) {
+  return ("You got " + score + " out of " + numQuestions + " correct. That is a " + Math.round((score / numQuestions) * 100) + '% score. <a href="/">Play again</a>.')
+}
+
+module.exports = {
+  bank: bank,
+  calculateScore:calculateScore,
+  scoreMessage:scoreMessage
+};
---
+++ server.js
@@ -0,0 +1,64 @@
+var http = require('http');
+var fs = require('fs');
+var formidable = require("formidable");
+var util = require('util');
+var bank = require('./assets/js/plugins').bank;
+var calculateScore = require('./assets/js/plugins').calculateScore;
+var scoreMessage = require('./assets/js/plugins').scoreMessage;
+
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static');
+
+var serve = serveStatic("./");
+
+var server = http.createServer(function (req, res) {
+    if (req.method.toLowerCase() == 'get'  && req.url == "/") {
+        displayForm(res);
+    } else if (req.method.toLowerCase() == 'post'  && req.url == "/score") {
+        processAllFieldsOfTheForm(req, res);
+    } else {
+      var done = finalhandler(req, res);
+      serve(req, res, done);
+    }
+
+});
+
+function displayForm(res) {
+    fs.readFile('index.html', function (err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+                'Content-Length': data.length
+        });
+        res.write(data);
+        res.end();
+    });
+}
+
+function processAllFieldsOfTheForm(req, res) {
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, fields, files) {
+      var userInput = [];
+
+      for (var key in fields) {
+        var value = fields[key];
+        userInput.push([key,value]);
+      }
+
+      var score = calculateScore(userInput,bank);
+      var message = scoreMessage(score,fields.numquestions || 10);
+
+      //Store the data from the fields in your data store.
+      //The data store could be a file or database or any other store based
+      //on your application.
+      res.writeHead(200, {
+        'content-type': 'text/html'
+      });
+      res.write('<h1>Your Score</h1>');
+      res.end('<p>' + message + '</p>');
+    });
+}
+
+
+server.listen(process.env.PORT || 1185);
+console.log("server listening on " + (process.env.PORT || 1185));
